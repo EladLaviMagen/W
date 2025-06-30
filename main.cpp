71e125f18bf50dea08cc523ea175a8cbf4443a48
Elad Magen <eladmagen10@gmail.com>
2025-06-30 17:19:24 +0300

    Better organized code, currently playing with thread security

--- main.cpp
+++ main.cpp
@@ -3,13 +3,37 @@
 
 
 int main() {
-	char str[] = "C:\\my_awesome_dll.dll";
-	LPVOID pathptr = str;
-	int pid = 9236;
-	HANDLE proc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
-	PVOID baseAddr = VirtualAllocEx(proc, pathptr, 1024, MEM_COMMIT, PAGE_READWRITE);
-	WriteProcessMemory(proc, baseAddr, pathptr, 22, NULL);
-	CreateRemoteThread(proc, NULL, 0, (LPTHREAD_START_ROUTINE)LoadLibrary, baseAddr, 0, NULL);
-	CloseHandle(proc);
+	CHAR str[] = "C:\\my_awesome_dll.dll";
+
+	//LPTHREAD_START_ROUTINE func = (LPTHREAD_START_ROUTINE)LoadLibraryA;
+	//func(str);
+
+	//HANDLE testThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, str, 0 ,NULL);
+	//WaitForSingleObject(testThread, INFINITE);
+
+	DWORD pid = 0;
+	scanf_s("%d", &pid);
+	HANDLE proc = OpenProcess(PROCESS_ALL_ACCESS, TRUE, pid);
+	LPVOID baseAddr = VirtualAllocEx(proc, NULL, strlen(str) + 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	SIZE_T written = 0;
+	
+	
+	BOOL check = WriteProcessMemory(proc, baseAddr, str, strlen(str) + 1, &written);
+
+	//THREAD_ALL_ACCESS 
+	HANDLE thread = CreateRemoteThread(proc, 
+										NULL,
+										0, 
+										(LPTHREAD_START_ROUTINE)LoadLibraryA, 
+										baseAddr, 
+										0,
+										NULL);
+
+	DWORD res = WaitForSingleObject(thread, INFINITE);
+	DWORD test = 0;
+	check = TerminateThread(thread, GetExitCodeThread(thread, &test));
+	check = CloseHandle(thread);
+	check = VirtualFreeEx(proc, baseAddr, 0, MEM_RELEASE);
+	check = CloseHandle(proc);
 	return 0;
 }
